23a4989e74ee99873e2609c723897d7414a7ea19
unbanksytv <unbanksytv@gmail.com>
2023-06-27 13:59:14 +0200

    first commit

---
+++ src/consts/parameters.ts
@@ -0,0 +1,13 @@
+
+export const contractConst = "0x7B4603c65909565aDdc91BB4B81696009b8847E4";
+
+export const chainConst = "mumbai";
+
+export const primaryColorConst = "teal";
+
+export const themeConst = "dark";
+
+// Gasless relayer configuration options
+export const relayerUrlConst = "https://api.defender.openzeppelin.com/autotasks/7378f7f3-be90-403b-8cc2-e014977f5c00/runs/webhook/aa080222-57c6-4eb2-8575-7ba295f1a9c1/7QC3TRvK4o3YTm5RA3YQ8L"; // OpenZeppelin relayer URL
+export const biconomyApiKeyConst = ""; // Biconomy API key
+export const biconomyApiIdConst = ""; // Biconomy API ID
